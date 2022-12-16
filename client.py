c307ba7560efa80ff2335b1f9edb18cb7615821d
Shpend Rashiti <92827452+shedyyy@users.noreply.github.com>
2022-12-16 23:55:51 +0100

    Update client.py

--- client.py
+++ client.py
@@ -11,7 +11,11 @@ class Client():
         self.__sock = socket.socket()
         self.__thread = None
 
-    def connect(self) -> int:
+    
+    def connection(self):
+            self.__sock.connect((self.__host, self.__port))
+
+    def connect(self):
         try :
             self.__sock.connect((self.__host,self.__port))
         except ConnectionRefusedError:
@@ -23,10 +27,6 @@ class Client():
         else :
             print ("Connection succesful")
             return 0
-
-    def connection(self):
-            self.__sock.connect((self.__host, self.__port))
-
 
     def communication(self, msg):
         self.__sock.send(msg.encode())
@@ -44,104 +44,70 @@ class MainWindow(QMainWindow):
         self.setCentralWidget(widget)
         grid = QGridLayout()
         widget.setLayout(grid)
-        self.__lab = QLabel("Host")
-        self.__lab8 = QLabel("Port")
-        self.__labadd = QLabel("New IP:")
-        self.__btnadd = QPushButton("Add")
-        self.__lab5 = QLabel("Command: ")
-        self.__lab9 = QLabel("Server response: ")
-        self.__info = QLabel("Informations \n( Host / Port )")
-        self.__lab2 = QLabel("")
+        self.__host = QLabel("Host")
+        self.__port = QLabel("Port")
+        self.__newip = QLabel("New IP:")
+        self.__addip = QPushButton("Add")
+        self.__entercommand = QLabel("Command: ")
+        self.__response = QLabel("Server response: ")
         self.__text10 = QLineEdit("")
-        self.__lab3 = QTextEdit(self)
-        self.__lab4 = QLabel("")
-        self.__labvide = QLabel("")
-        self.__text = QComboBox()
-        self.__text2 = QLineEdit("")
-        self.__text3 = QLineEdit("")
-        self.__text11 = QLineEdit("")
-        self.__savefichier = QLineEdit("")
-        self.__infohost = QLineEdit("")
-        self.__infoport = QLineEdit("")
-        self.__infoportlabel = QLabel("")
-        self.__infohostlabel = QLabel("")
+        self.__entertext = QTextEdit(self)
+        self.__iptext = QComboBox()
+        self.__porttext = QLineEdit("")
+        self.__commandstext = QLineEdit("")
+        self.__filetext = QLineEdit("")
+        self.__savefile = QLineEdit("")
         self.__text10.hide()
-        self.__labadd.hide()
-        self.__btnadd.hide()
-        self.__savefichier.hide()
-        self.__btnadd.setEnabled(False)
+        self.__newip.hide()
+        self.__addip.hide()
+        self.__savefile.hide()
+        self.__addip.setEnabled(False)
         self.__w = None
 
 
-
-
         self.__client = None
-        self.__okcon = QPushButton("Connect")
-        self.__okcom = QPushButton("Send")
-        self.__newconnection = QPushButton("New connection")
-        self.__fichiername = QPushButton("Choose")
+        self.__connectbutton = QPushButton("Connect")
+        self.__send = QPushButton("Send")
+        self.__filename = QPushButton("Choose")
 
 
-        self.shortcut_open = QShortcut(QKeySequence('Return'), self)
-        self.shortcut_open.activated.connect(self.command1)
-        self.shortcut_open2 = QShortcut(QKeySequence('Enter'), self)
-        self.shortcut_open2.activated.connect(self.command1)
-
-
-        # Using readlines()
-        self.__text11.setReadOnly(True)
-        self.__lab3.hide()
-        self.__lab5.hide()
-        self.__text3.hide()
+        self.__filetext.setReadOnly(True)
+        self.__entertext.hide()
+        self.__entercommand.hide()
+        self.__commandstext.hide()
         self.__labnomfichier = QLabel("Source file:")
-        grid.addWidget(self.__info, 0, 0)
-        grid.addWidget(self.__labadd, 9, 0)
-        grid.addWidget(self.__labvide, 6, 0)
-        grid.addWidget(self.__btnadd, 9, 2)
-        grid.addWidget(self.__infohost, 0, 1)
-        grid.addWidget(self.__infoport, 0, 2)
-        grid.addWidget(self.__infoportlabel, 0, 2)
-        grid.addWidget(self.__infohostlabel, 0, 1)
-        grid.addWidget(self.__lab3, 3, 0, 1, 5)
-        grid.addWidget(self.__lab2, 4, 1)
-        grid.addWidget(self.__lab4, 1, 1)
-        grid.addWidget(self.__lab, 2, 0)
-        grid.addWidget(self.__lab8, 3, 0)
-        grid.addWidget(self.__lab5, 2, 0)
-        grid.addWidget(self.__lab9, 3, 0)
-        grid.addWidget(self.__text, 2, 1)
-        grid.addWidget(self.__text2, 3, 1)
+        grid.addWidget(self.__newip, 9, 0)
+        grid.addWidget(self.__addip, 9, 2)
+        grid.addWidget(self.__entertext, 3, 0, 1, 5)
+        grid.addWidget(self.__host, 2, 0)
+        grid.addWidget(self.__port, 3, 0)
+        grid.addWidget(self.__entercommand, 2, 0)
+        grid.addWidget(self.__response, 3, 0)
+        grid.addWidget(self.__iptext, 2, 1)
+        grid.addWidget(self.__porttext, 3, 1)
         grid.addWidget(self.__text10, 9, 1)
-        self.__text3.setPlaceholderText("Enter your command here!")
-        grid.addWidget(self.__text3, 2, 1, 1, 3)
-        self.__text2.setText("10000")
-        self.__text3.setText("")
+        self.__commandstext.setPlaceholderText("Enter your command here!")
+        grid.addWidget(self.__commandstext, 2, 1, 1, 3)
+        self.__porttext.setText("10000")
+        self.__commandstext.setText("")
         self.__text10.setPlaceholderText("Enter a new IP here")
-        self.__lab3.setReadOnly(True)
-        grid.addWidget(self.__okcon, 4, 1)
-        grid.addWidget(self.__okcom, 2, 4)
-        grid.addWidget(self.__newconnection, 0, 3)
+        self.__entertext.setReadOnly(True)
+        grid.addWidget(self.__connectbutton, 4, 1)
+        grid.addWidget(self.__send, 2, 4)
         grid.addWidget(self.__labnomfichier, 8, 0)
-        grid.addWidget(self.__text11, 8, 1)
-        grid.addWidget(self.__fichiername, 8, 2)
-        grid.addWidget(self.__savefichier, 10, 2)
+        grid.addWidget(self.__filetext, 8, 1)
+        grid.addWidget(self.__filename, 8, 2)
+        grid.addWidget(self.__savefile, 10, 2)
 
 
 
-        self.__info.hide()
-        self.__infohost.hide()
-        self.__infoport.hide()
-        self.__infoportlabel.hide()
-        self.__infohostlabel.hide()
-        self.__newconnection.hide()
-        self.__okcon.clicked.connect(self.okconnexion)
-        self.__okcom.clicked.connect(self.command1)
-        self.__newconnection.clicked.connect(self.newconnection)
-        self.__btnadd.clicked.connect(self.ajout)
-        self.__fichiername.clicked.connect(self.fichiernom)
-        self.__okcom.hide()
-        self.__lab9.hide()
-        self.__okcon.setEnabled(False)
+        self.__connectbutton.clicked.connect(self.connectbuttonnexion)
+        self.__send.clicked.connect(self.command1)
+        self.__addip.clicked.connect(self.ajout)
+        self.__filename.clicked.connect(self.filename)
+        self.__send.hide()
+        self.__response.hide()
+        self.__connectbutton.setEnabled(False)
 
 
 
@@ -149,37 +115,37 @@ class MainWindow(QMainWindow):
 
 
     def command1(self):
-        msg = self.__text3.text()
+        msg = self.__commandstext.text()
         try:
             reponse = self.__client.communication(msg)
         except:
-            self.__lab3.append(f"The server is off or you aren't connected anymore! Please verify your settings\n")
+            self.__entertext.append(f"The server is off or you aren't connected anymore! Please verify your settings\n")
         else:
-            self.__lab3.append(f"{reponse}\n")
-            self.__text3.clear()
+            self.__entertext.append(f"{reponse}\n")
+            self.__commandstext.clear()
 
 
 
 
 
-    def fichiernom(self):
+    def filename(self):
         try:
             options = QFileDialog.Options()
             options |= QFileDialog.DontUseNativeDialog
             fileName, _ = QFileDialog.getOpenFileName(self, "Choose your file", "", "Text files (*.txt)", options=options)
             testest = pathlib.Path(fileName).name
-            self.__text11.setText(fileName)
+            self.__filetext.setText(fileName)
             file1 = open(f"{testest}", 'r')
             Lines = file1.readlines()
             count = 0
             for line in Lines:
                 count += 1
-                self.__text.addItem(line.strip())
-            self.__okcon.setEnabled(True)
-            self.__btnadd.setEnabled(True)
+                self.__iptext.addItem(line.strip())
+            self.__connectbutton.setEnabled(True)
+            self.__addip.setEnabled(True)
             self.__text10.show()
-            self.__labadd.show()
-            self.__btnadd.show()
+            self.__newip.show()
+            self.__addip.show()
         except:
             msg = QMessageBox()
             msg.setWindowTitle("Error")
@@ -188,80 +154,57 @@ class MainWindow(QMainWindow):
             x = msg.exec_()
 
 
-    def newconnection(self):
-        self.__w = MainWindow()
-        self.__w.show()
 
 
 
 
-    def okconnexion(self):
+    def connectbuttonnexion(self):
         try:
-            host = str(self.__text.currentText())
-            port = int(self.__text2.text())
+            host = str(self.__iptext.currentText())
+            port = int(self.__porttext.text())
             self.__client = Client(host, port)
             self.__client.connection()
         except:
             msg = QMessageBox()
-            msg.setWindowTitle("Erreur")
+            msg.setWindowTitle("ERROR")
             msg.setText("Connection is not possible! Make sure the server is ON or that you have entered the right IP.")
             msg.setIcon(QMessageBox.Critical)
             x = msg.exec_()
         else:
-            self.__okcon.setEnabled(False)
-            self.__okcom.show()
-            self.__lab3.show()
-            self.__lab5.show()
-            self.__infohost.show()
-            self.__infoport.show()
-            self.__infoport.setEnabled(False)
-            self.__infohost.setEnabled(False)
-            self.__infoportlabel.show()
-            self.__infohostlabel.show()
-            self.__text3.show()
-            self.__text2.hide()
-            self.__lab.hide()
-            self.__btnadd.hide()
-            self.__labadd.hide()
-            self.__info.show()
-            self.__lab8.hide()
-            # self.__lab9.show()
-            self.__text2.hide()
+            self.__connectbutton.setEnabled(False)
+            self.__send.show()
+            self.__entertext.show()
+            self.__entercommand.show()
+            self.__commandstext.show()
+            self.__porttext.hide()
+            self.__host.hide()
+            self.__addip.hide()
+            self.__newip.hide()
+            self.__port.hide()
+            self.__porttext.hide()
             self.__text10.hide()
-            self.__okcon.hide()
-            self.__newconnection.show()
-            self.__text11.hide()
+            self.__connectbutton.hide()
+            self.__filetext.hide()
             self.__labnomfichier.hide()
-            self.__fichiername.hide()
-            self.__infoportlabel.setText(f" {self.__text2.text()}")
-            self.__infohostlabel.setText(f" {self.__text.currentText()}")
+            self.__filename.hide()
 
     def ajout(self):
-        if self.__text10.text() != "":
-            testest = self.__text11.text()
+        if self.__text10.iptext() != "":
+            testest = self.__filetext.iptext()
             file = open(f"{testest}", "a")
             file.write(f"\n{self.__text10.text()}")
-            self.__text.addItem(self.__text10.text())
+            self.__iptext.addItem(self.__text10.text())
             self.__text10.setText("")
-            self.__savefichier = self.__text11.text()
+            self.__savefile = self.__filetext.text()
         else:
             msg = QMessageBox()
-            msg.setWindowTitle("Erreur")
-            msg.setText("Impossible d'ajouter un host vide")
+            msg.setWindowTitle("ERROR")
+            msg.setText("Please add an IP")
             msg.setIcon(QMessageBox.Critical)
             x = msg.exec_()
 
-
-
-
-
-
     def _actionQuitter(self):
         QCoreApplication.exit(0)
-
-
-
-
 
 
 if __name__ == "__main__":
